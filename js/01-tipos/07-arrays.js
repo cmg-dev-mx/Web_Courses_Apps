2c63c99973e00be04e3df7ce636afb8a43603518
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-18 21:04:41 -0600

    Ejemplo de arreglos

---
+++ js/01-tipos/07-arrays.js
@@ -0,0 +1,14 @@
+let animales = ['Perro', 'Gato', 'Conejo', 'Pez', 'Pájaro'];
+console.log(animales);
+console.log(animales[0]);
+
+animales[5] = 'Tortuga';
+console.log(animales);
+
+animales[10] = 'Serpiente';
+console.log(animales);
+console.log(animales[7]);
+
+console.log(typeof animales);
+
+console.log(animales.length);
