0c4409dcaaac6fbec2c569c3aaeab948e5fa48f1
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-19 20:12:47 -0600

    Ejemplo de flujo ELSE

---
+++ js/03-control-flujo/02-else.js
@@ -0,0 +1,8 @@
+let edad = 16;
+if (edad > 17) {
+    console.log('Usuario mayor de edad');
+} else if (edad > 13) {
+    console.log("Usuario necesita estar acompañado de un adulto");
+} else {
+    console.log('Usuario menor de edad');
+}
