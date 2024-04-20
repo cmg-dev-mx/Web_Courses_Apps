ab9975c97d9e3445fa35474c2324e29e3f8bbaf1
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-19 19:56:15 -0600

    Ejemplo de operadores bitwise

---
+++ js/02-operadores/06-bitwise.js
@@ -0,0 +1,16 @@
+// bit: 0 1
+// Byte: 8 bits (00000000) -> 0
+//              (00000001) -> 1
+//              (00000010) -> 2
+//              (00000011) -> 3
+
+// Decimal: 0 1 2 3 4 5 6 7 8 9
+// Binario: 0 1
+
+console.log(1 | 3);
+console.log(1 | 4);
+console.log(3 | 5);
+
+console.log(1 & 3);
+console.log(1 & 4);
+console.log(3 & 5);
