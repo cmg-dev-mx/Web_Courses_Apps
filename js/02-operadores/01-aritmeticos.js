2f2237c49e81b8d09a35d272d78d88381e49a86a
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-19 19:29:48 -0600

    Ejemplo de operadores aritméticos

---
+++ js/02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b, 'adicion');
+console.log(a - b, 'resta');
+console.log(a * b, 'multiplicación');
+console.log(a / b, 'división');
+console.log(a % b, 'modulo'); 
+console.log(a ** b, 'potencia');
+
+// Incremento
+
+let c = 5;
+c++;
+console.log(c, 'incremento');
+
+// Decremento
+
+let d = 5;
+d--;
+console.log(d, 'decremento');
