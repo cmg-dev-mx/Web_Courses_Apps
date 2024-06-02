05005c5060c5364c7d177d91fbe69f27cf8c4d63
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-06-02 16:01:43 -0600

    Funciones como propiedades

--- react/react-app/src/App.tsx
+++ react/react-app/src/App.tsx
@@ -3,12 +3,15 @@ import List from "./components/List";
 
 function App() {
   const list = ["Goku", "Vegeta", "Gohan", "Trunks", "Goten"];
+  const handleSelect = (elemento: string) => {
+    console.log(`Elemento seleccionado: ${elemento}`);
+  };
 
   // JSX
   return (
     <Card>
       <CardBody title="Hola Mundo" content="Este es el texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
     </Card>
   );
 }
