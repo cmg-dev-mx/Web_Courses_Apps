809a0996cb63726b2a52a4c8cb43d29a9a49ed8f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-06-02 16:12:43 -0600

    Ejemplo de truthly y falsy en React

--- react/react-app/src/App.tsx
+++ react/react-app/src/App.tsx
@@ -3,6 +3,7 @@ import List from "./components/List";
 
 function App() {
   const list = ["Goku", "Vegeta", "Gohan", "Trunks", "Goten"];
+  const list2: string[] = [];
   const handleSelect = (elemento: string) => {
     console.log(`Elemento seleccionado: ${elemento}`);
   };
@@ -10,6 +11,12 @@ function App() {
   // JSX
   return (
     <Card>
+      {/* {"" && "string vacio"}
+      {undefined && "string indefinido"}
+      {null && "string nulo"}
+      {false && "falso"}
+      {0 && "numero cero"} */}
+      {list2.length !== 0 && "Mi lista"}
       <CardBody title="Hola Mundo" content="Este es el texto" />
       <List data={list} onSelect={handleSelect} />
     </Card>
